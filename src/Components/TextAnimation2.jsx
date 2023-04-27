308c2a25b791ae99adadba464e3b9a36af262a58
Jaydeep <khachariyajaydeep@gmail.com>
2023-04-28 01:54:56 +0530

    after animation change left to right

--- src/Components/TextAnimation2.jsx
+++ src/Components/TextAnimation2.jsx
@@ -4,15 +4,15 @@ export default function TextAnimation2() {
   return (
     <section className="textSection animate__animated animate__fadeInUpBig">
       <div className="textAniSection">
-        <p className="animate__animated animate__fadeInLeftBig animate__delay-2s">
+        <p className="animate__animated animate__fadeInRightBig animate__delay-2s">
           ITS TIME TO
         </p>
         <br />
-        <p className="animate__animated animate__fadeInLeftBig animate__delay-3s">
+        <p className="animate__animated animate__fadeInRightBig animate__delay-3s">
           MAKE A
         </p>
         <br />
-        <p className="animate__animated animate__fadeInLeftBig animate__delay-4s">
+        <p className="animate__animated animate__fadeInRightBig animate__delay-4s">
           DIFFERENCE
         </p>
         {/* <br /> <br />
