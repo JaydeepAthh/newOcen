47f925a91554cc7fd918e431e640e254685601d5
Jaydeep <khachariyajaydeep@gmail.com>
2023-05-07 18:05:40 +0530

    Changes in team section

--- src/Components/Team.jsx
+++ src/Components/Team.jsx
@@ -114,7 +114,7 @@ export default function Team() {
               </p>
             </div>
             <div>
-              <h1 className="number">04</h1>
+              <h1 className="number">05</h1>
               <img className="img" src={member4} alt="member4" />
             </div>
           </div>
@@ -132,7 +132,7 @@ export default function Team() {
               </p>
             </div>
             <div>
-              <h1 className="number">05</h1>
+              <h1 className="number">06</h1>
               <img className="img" src={member5} alt="member5" />
             </div>
           </div>
